5a60cb54b40d9c7bf9e7297f16065a16fb420caf
everwinner64 <evango64off@gmail.com>
2026-08-03 10:55:12 +0200

    improved CSP support

--- 404.js
+++ 404.js
@@ -2,15 +2,15 @@ import Typewriter from "typewriter-effect/dist/core";
 
 const terminal = document.getElementById('terminal-output');
 
-const typewriter = new Typewriter(terminal, { loop: true, delay: 75 });
+const typewriter = new Typewriter(terminal, { loop: true, delay: 75, skipAddStyles: true });
 
 typewriter
     .pauseFor(1000)
     .typeString('dnv http status 404')
-    .pasteString("<br><span style='color: #0000FF;'>HTTP Status Code:</span> 404")
-    .pasteString("<br><span style='color: #0000FF;'>Name:</span> Not Found")
-    .pasteString("<br><span style='color: #0000FF;'>Meaning:</span> The server cannot find the requested resource. The URL may be incorrect or the resource may have been removed.")
-    .pasteString("<br><span style='color: #0000FF;'>Usage:</span> Used when the requested resource could not be found on the server.")
+    .pasteString("<br><span class='c-blue'>HTTP Status Code:</span> 404")
+    .pasteString("<br><span class='c-blue'>Name:</span> Not Found")
+    .pasteString("<br><span class='c-blue'>Meaning:</span> The server cannot find the requested resource. The URL may be incorrect or the resource may have been removed.")
+    .pasteString("<br><span class='c-blue'>Usage:</span> Used when the requested resource could not be found on the server.")
     .pauseFor(4000)
     .deleteAll(30)
     .start();
